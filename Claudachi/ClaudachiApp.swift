0372efe7abb62f50e71a9cc94a5b0a886380134b
gonzchris <hello@gonzalezchris.com>
2026-01-03 18:17:34 -0800

    Initial Commit

---
+++ Claudachi/ClaudachiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudachiApp.swift
+//  Claudachi
+//
+//  Created by Chris Gonzalez on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudachiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
